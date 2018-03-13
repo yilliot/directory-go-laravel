ce47cf0dccfdd2c68c1b7ed0eacd0a22ddbadd2a
Elliot Yap <yilliot@gmail.com>
2018-03-14 01:52:08 +0800

    view skeleton

---
+++ resources/views/office/area/index.blade.php
@@ -0,0 +1,22 @@
+mkdir resources/views/area
+touch resources/views/area/index.blade.php
+touch resources/views/area/list.blade.php
+touch resources/views/area/create.blade.php
+touch resources/views/area/edit.blade.php
+
+mkdir resources/views/zone
+touch resources/views/zone/index.blade.php
+touch resources/views/zone/list.blade.php
+touch resources/views/zone/create.blade.php
+touch resources/views/zone/edit.blade.php
+
+mkdir resources/views/level
+touch resources/views/level/index.blade.php
+touch resources/views/level/list.blade.php
+
+mkdir resources/views/category
+touch resources/views/category/index.blade.php
+touch resources/views/category/list.blade.php
+touch resources/views/category/create.blade.php
+touch resources/views/category/edit.blade.php
+
