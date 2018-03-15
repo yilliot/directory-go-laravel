d17fd23931bd177748c42ef51d4db9e97c07f54e
Elliot Yap <yilliot@gmail.com>
2018-03-15 18:50:30 +0800

    nested category areas and zones

--- app/Http/Controllers/KioskController.php
+++ app/Http/Controllers/KioskController.php
@@ -8,7 +8,51 @@ class KioskController extends Controller
 {
     function index()
     {
-        $blocks = \App\Models\Block::with('levels', 'levels.areas', 'levels.zones')->get()->toJson();
+        $blocks = \App\Models\Block::with('levels')->get();
+
+        $areasByCategories = \DB::table('areas')
+            ->join('area_categories', 'areas.id', '=', 'area_categories.area_id')
+            ->join('categories', 'categories.id', '=', 'area_categories.category_id')
+            ->select('areas.*', 'area_categories.category_id', 'categories.name as category_name')
+            ->get()
+            ->groupBy('level_id')
+            ->map(function($items, $levelId){
+                return $items->groupBy('category_id')
+                    ->map(function($items, $id){
+                        return collect([
+                            'id' => $id,
+                            'name' => $items->first()->category_name,
+                            'areas' => $items,
+                        ]);
+                    });
+            });
+
+        $zonesByCategories = \DB::table('zones')
+            ->join('zone_categories', 'zones.zone_category_id', '=', 'zone_categories.id')
+            ->select('zones.*', 'zone_categories.name as category_name')
+            ->get()
+            ->groupBy('level_id')
+            ->map(function($items, $levelId){
+                return $items->groupBy('zone_category_id')
+                    ->map(function($items, $id){
+                        return collect([
+                            'id' => $id,
+                            'name' => $items->first()->category_name,
+                            'zones' => $items,
+                        ]);
+                    });
+            });
+
+        $blocks = $blocks->map(function($block) use ($areasByCategories, $zonesByCategories) {
+
+            return $block->levels->map(function($level) use ($areasByCategories, $zonesByCategories) {
+                $level = $level->toArray();
+                $level['area_categories'] = $areasByCategories->get($level['id']);
+                $level['zone_categories'] = $zonesByCategories->get($level['id']);
+                return $level;
+            });
+        });
+
         return view('index', compact('blocks'));
     }
 }
