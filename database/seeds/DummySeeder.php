2d9b27eea122d69e153ef31b88d624366b8ecb1e
Elliot Yap <yilliot@gmail.com>
2018-03-15 17:36:09 +0800

    category relationship

--- database/seeds/DummySeeder.php
+++ database/seeds/DummySeeder.php
@@ -11,7 +11,12 @@ class DummySeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\Area::class, 1000)->create();
-        factory(\App\Models\Zone::class, 1000)->create();
+        factory(\App\Models\Area::class, 300)->create()
+            ->each(function($area) {
+                $randomCategory = \App\Models\Category::find(rand(1, 23));
+                $area->categories()->attach($randomCategory);
+            });
+        factory(\App\Models\Zone::class, 60)->create();
+
     }
 }
