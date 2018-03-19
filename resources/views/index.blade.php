d4e41a79cfbd4b885027385de39a8b050e030444
Khoo Yong Jun <khoo.jack@gmail.com>
2018-03-20 00:29:13 +0800

    database name changing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,9 @@
 @section('script')
 <script>
 let blocks = {!!$blocks!!};
+for(let i in blocks.blocks) {
+    blocks.blocks[i].colour = blocks.blocks[i].bg_colour;
+}
 let direction = 0;
 </script>
 <script src="/js/ui.js"></script>
